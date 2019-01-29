b32860f8bb9df0c92e734484eedd556e489f972e
ataricoder <ataricoder@gmail.com>
2019-01-28 23:37:26 -0600

    done with main functionality of the app

--- tippy/CalculatorViewController.swift
+++ tippy/CalculatorViewController.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
     @IBAction func onTap(_ sender: Any) {
-        print("Hello");
+        view.endEditing(true)
     }
     
-
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        //Get the bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        //Calcualte the tip and total
+        let tip = bill * 0.1
+        let total = bill + tip
+        
+        //Update the tip and total labels
+        tipLabel.text = "$\(tip)"
+        totalLabel.text="$\(total)"
+    }
+    
 }
