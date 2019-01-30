fdbff5b623484675cea61dabea5f7b603c9df992
ataricoder <ataricoder@gmail.com>
2019-01-30 00:47:34 -0600

    figured out split functionality, added outside function, now working

--- tippy/CalculatorViewController.swift
+++ tippy/CalculatorViewController.swift
@@ -14,6 +14,7 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitControl: UISlider!
     @IBOutlet weak var splitField: UILabel!
     
     override func viewDidLoad() {
@@ -23,31 +24,34 @@ class CalculatorViewController: UIViewController {
         self.title = "Tippy"
     }
     
-    @IBAction func onTap(_ sender: Any) {
-        view.endEditing(true)
-    }
     
-    @IBAction func calculateTip(_ sender: Any) {
-        
-        let numberPeople = Double(splitField.text!) ?? 0
-        
-        //Get the bill amount
+    //main function to calculate bill, called in actions
+    func calculateBill() {
         let bill = Double(billField.text!) ?? 0
-        
-        //Calcualte the tip and total
         let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = (bill + tip)/numberPeople
+        let split = Int(splitControl.value)
+        let total = (bill + tip)/Double(split)
         
-        
-        
-        //Update the tip and total labels
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
     
+    //tap anywhere to dismiss keyboard
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    //update total, and the number of people for split
     @IBAction func calculateSplit(_ sender: UISlider) {
         splitField.text! = String(Int(sender.value))
-        
+        calculateBill()
     }
+    
+    //update total, if tip percentage change
+    @IBAction func calculateTip(_ sender: Any) {
+        calculateBill()
+    }
+    
+
 }
